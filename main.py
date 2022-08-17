4a502e24ff63705b201607de8e24c4b33389d243
KhushdeepKaurNagi <khushdeep.nagi@intangles.com>
2022-08-17 12:20:00 +0530

    oops assignment

---
+++ main.py
@@ -0,0 +1,34 @@
+class Department:
+    def __init__(self):
+        self.fin = self.Finance()
+
+    def display(self):
+        print('In Department Class')
+
+    class Finance:
+
+        def display1(self):
+            print("Finance class")
+
+
+class Employee(Department):
+    def __init__(self):
+        print('In Subclass')
+        super().__init__()
+
+    class Finance(Department.Finance):
+
+        def display2(self):
+            print('Employee finance')
+
+        # creating Sub_class object
+
+
+a = Employee()
+a.display()
+
+# creating inner class object
+b = a.fin
+b.display1()
+b.display2()
+
